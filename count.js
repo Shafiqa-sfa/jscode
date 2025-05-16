c9437073dce37984dbfef1e12ac431e52ec2d60f
Shafiqa-sfa <shafiqahairun21@gmail.com>
2025-05-16 11:57:51 +0530

    first commit

---
+++ count.js
@@ -0,0 +1,27 @@
+
+
+ 
+        setTimeout(()=>{
+        document.getElementById("count").innerHTML=5
+        setTimeout(()=>{
+        document.getElementById("count").innerHTML=4
+        setTimeout(()=>{
+        document.getElementById("count").innerHTML=3
+        setTimeout(()=>{
+        document.getElementById("count").innerHTML=2
+        setTimeout(()=>{
+        document.getElementById("count").innerHTML=1
+        setTimeout(()=>{
+        document.getElementById("count").innerHTML="You can Do It!! 💥💥💥"
+        
+    },1000)
+        
+    },1000)
+        
+    },1000)
+        
+    },1000)
+        
+    },3000)
+        
+    },1000)
