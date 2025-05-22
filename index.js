ec788aa036911a64248279abb60e9460bc1294bd
Shafiqa-sfa <shafiqahairun21@gmail.com>
2025-05-22 15:55:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config()
+
+import cors from "cors";
+import connectDB from "./database/connectDB.js";
+import userRoute from "./routes/userRoutes.js";
+
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/user",userRoute)
+
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"API changed successfully"})
+});
+app.get("/api/heart-beat",(req,res)=>{
+    res.status(200).json({data:"Heartbeat was successful"});
+    console.log("Heart beat was successful")
+});
+console.log(process.env.PORT)
+app.listen(process.env.PORT,()=>{
+    console.log(`server running successfully at port ${process.env.PORT}`);
+});
+connectDB()
